35efa94c636caaa80a0a8fdad84985c3f174b13b
Mathi Fonseca <mathifonseca@gmail.com>
2014-10-08 13:16:37 -0200

    deleting parenthesis in BuildConfig causes problems

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:2.0.3' {
+        build(':release:3.0.1', ':rest-client-builder:2.0.3') {
             export = false
         }
         provided(':version-update:1.3.2') {
